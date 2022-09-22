235393b3676d78ae7eb4500500d94d638643b6a6
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2022-09-22 09:47:09 +0100

    nextjs, tailwind and typescript config

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+	return (
+		<div className='w-screen  h-screen  flex justify-center items-center'>
+			<h1 className='text-xl'>Value Female Network</h1>
+		</div>
+	);
+};
+
+export default Home;
